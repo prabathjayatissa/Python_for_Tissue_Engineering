091c19e353733aa210a55ac3c1a9d27ab39fb791
Dr Prabath jayatissa <prabathjayatissa1905@gmail.com>
2026-07-08 15:03:23 +0200

    new Files

---
+++ numpy_2.py
@@ -0,0 +1,10 @@
+"""
+==============================================================
+NUMPY MASTERCLASS - 2
+For MSc Tissue Engineering Students (FHTW)
+
+Author: Dr Prabath Jayathissa
+Run section by section.
+
+==============================================================
+"""
